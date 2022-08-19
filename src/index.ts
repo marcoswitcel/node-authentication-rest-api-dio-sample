e1f26e1459964a43df170e7b5e104a5608d00f0c
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-19 20:50:14 -0300

    post, json suporte e outro middleware

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Configurações
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cadastrando rotas
 app.use(usersRoute);
 
 app.use('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ status: 'UP' });
 });
 
+
+// Inicializando o servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta: ' + 3000);
 })
