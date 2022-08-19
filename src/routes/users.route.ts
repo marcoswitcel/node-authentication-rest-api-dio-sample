d011a64ddd5700e7ffe69662403542d30000b645
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-19 20:38:07 -0300

    usando status code

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{}];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    res.status(200).send(req.params.uuid);;
+    res.status(StatusCodes.OK).send(req.params.uuid);;
 });
 
 export default usersRoute;
