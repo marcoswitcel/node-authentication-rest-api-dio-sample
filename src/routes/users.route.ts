f1c7a892905685990f4ee1f53a7f57dba46435e3
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-19 21:00:47 -0300

    put e delete do usuário

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -17,4 +17,14 @@ usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
     res.status(StatusCodes.CREATED).send(newUser);
 });
 
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const user = req.body;
+    res.status(StatusCodes.OK).send({ ...user, uuid });
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+});
+
 export default usersRoute;
