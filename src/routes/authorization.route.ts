754348858a058dc123afe02fd004c98bf387f588
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-09-02 22:45:03 -0300

    geração de token 100% funcional

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,12 +1,28 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import JWT from 'jsonwebtoken';
+import UnauthorizedError from '../errors/unauthorized.error';
 import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
 
 const authorizationRoute = Router();
 
 authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
-    // @TODO João, implementar endpoint que gera o token
-    res.status(StatusCodes.OK).json({ 'message' : 'passei pelo basic' });
+    try {
+        const user = req.user;
+
+        if (!user) {
+            throw new UnauthorizedError('Usuário não associado!');
+        }
+
+        const payload = { username: user.username };
+        const options = { subject: user.uuid };
+        const secretKey = 'minha_chave_secreta';
+        const jwtToken = JWT.sign(payload, secretKey, options);
+
+        res.status(StatusCodes.OK).json({ token: jwtToken })
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default authorizationRoute;
