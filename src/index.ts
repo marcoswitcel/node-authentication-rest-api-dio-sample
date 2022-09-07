7bcbf3a1737b19dc9d362c758b3d6bd824dab6a1
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-09-07 14:22:07 -0300

    sigterm implementado

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import databaseConnection from './data-source/database-connection';
 import globalErrorHandler from './middlewares/global-error-handler.middleware';
 import authorizationRoute from './routes/authorization.route';
 import statusRoute from './routes/status.route';
@@ -19,6 +20,16 @@ app.use(authorizationRoute);
 app.use(globalErrorHandler);
 
 // Inicializando o servidor
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log('Aplicação executando na porta: ' + 3000);
-})
+});
+
+// Operações executadas no encerramento do programa
+process.on('SIGTERM', () => {
+    databaseConnection.end(() => {
+        console.log('Conexão com a base de dados encerrada!');
+    });
+    server.close(() => {
+        console.log('Aplicação encerrada');
+    });
+});
