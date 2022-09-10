58199dad32b218e76c80cce70ea0fa8d7a0ba639
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-09-09 21:55:01 -0300

    middleware de autenticação JWT

--- src/middlewares/jwt-authentication.middleware.ts
+++ src/middlewares/jwt-authentication.middleware.ts
@@ -1,11 +1,49 @@
+import config from 'config';
 import { NextFunction, Request, Response } from 'express';
+import UnauthorizedError from '../errors/unauthorized.error';
+import JWT from 'jsonwebtoken';
+import { userRepository } from '../repositories';
+
+const secretKey = config.get<string>('authentication.secretKey');
 
 async function jwtAuthenticationMiddleware(req: Request, res: Response, next: NextFunction) {
-    /**
-     * @TODO João, terminar de implementar o middleware que checa a presença de token e
-     * permite a continuação da request
-     */
-    next();
+    try {
+        const authorizationHeader = req.headers.authorization;
+
+        if (typeof authorizationHeader === 'undefined') {
+            throw new UnauthorizedError('Header "authorization" não presente');
+        } 
+
+        const [type, jwtToken] = authorizationHeader.split(' ');
+
+        if (type !== 'Bearer') {
+            throw new UnauthorizedError('Tipo de autenticação não suportado');
+        }
+
+        if (!jwtToken) {
+            throw new UnauthorizedError('JWT token não presente');
+        }
+
+        try {
+            const payload = JWT.verify(jwtToken, secretKey);
+            if (typeof payload !== 'object' || !payload.sub) {
+                throw new UnauthorizedError('JWT token inválido')
+            }
+
+            const user = await userRepository.findById(payload.sub);
+
+            if (!user) {
+                throw new UnauthorizedError('Usuário associado ao JWT token não encontrado')
+            }
+
+            req.user = user;
+            next();
+        } catch (error) {
+            throw (error instanceof UnauthorizedError) ? error : new UnauthorizedError('JWT token inválido, "sub" mal formatado');
+        }
+    } catch (error) {
+        next(error);
+    }
 }
 
 export default jwtAuthenticationMiddleware;
